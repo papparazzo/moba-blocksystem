7d2d750d68ff36a2ece52233a839c057c0ebb5d6
papparazzo <pappi-@gmx.de>
2019-03-24 00:17:45 +0100

    wip...

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+/*
+ *  Project:    moba-blocksystem
+ *
+ *  Copyright (C) 2019 Stefan Paproth <pappi-@gmx.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+#include <iostream>
+#include <functional>
+#include <memory>
+#include <thread>
+
+#include <moba/helper.h>
+#include <moba/log.h>
+
+#include "config.h"
+
+#include "moba/socket.h"
+#include "moba/endpoint.h"
+
+namespace {
+    moba::AppData appData = {
+        PACKAGE_NAME,
+        moba::Version(PACKAGE_VERSION),
+        __DATE__,
+        __TIME__,
+        "::1",
+        7000
+    };
+}
+
+int main(int argc, char *argv[]) {
+    moba::setCoreFileSizeToULimit();
+
+    auto groups = std::make_shared<moba::JsonArray>();
+    groups->push_back(moba::toJsonStringPtr("SYSTEM"));
+
+    auto socket = std::make_shared<Socket>(appData.host, appData.port);
+    auto endpoint = std::make_shared<Endpoint>(socket, appData.appName, appData.version, groups);
+    endpoint->connect();
+
+    return EXIT_SUCCESS;
+}
