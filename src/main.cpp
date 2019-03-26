3969af9206f9fefd69a3291b5be1050b3b340311
papparazzo <pappi-@gmx.de>
2019-03-26 22:17:47 +0100

    wip...

--- src/main.cpp
+++ src/main.cpp
@@ -46,11 +46,14 @@ int main(int argc, char *argv[]) {
     moba::setCoreFileSizeToULimit();
 
     auto groups = std::make_shared<moba::JsonArray>();
-    groups->push_back(moba::toJsonStringPtr("SYSTEM"));
+    groups->push_back(moba::toJsonStringPtr("LAYOUT"));
+    groups->push_back(moba::toJsonStringPtr("INTERFACE"));
 
     auto socket = std::make_shared<Socket>(appData.host, appData.port);
     auto endpoint = std::make_shared<Endpoint>(socket, appData.appName, appData.version, groups);
     endpoint->connect();
+
+    
 
     return EXIT_SUCCESS;
 }
